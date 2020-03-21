0b2ed4288d26489f06a0e6e61498cf4e6fd0dae0
GuikangZhong <48389832+GuikangZhong@users.noreply.github.com>
2020-03-21 12:39:58 -0400

    Add files via upload

---
+++ readimage.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include "ext2.h"
+
+unsigned char *disk;
+
+
+int main(int argc, char **argv) {
+
+    if(argc != 2) {
+        fprintf(stderr, "Usage: readimg <image file name>\n");
+        exit(1);
+    }
+    int fd = open(argv[1], O_RDWR);
+
+    disk = mmap(NULL, 128 * 1024, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if(disk == MAP_FAILED) {
+	perror("mmap");
+	exit(1);
+    }
+
+    struct ext2_super_block *sb = (struct ext2_super_block *)(disk + 1024);
+    printf("Inodes: %d\n", sb->s_inodes_count);
+    printf("Blocks: %d\n", sb->s_blocks_count);
+    
+    return 0;
+}
